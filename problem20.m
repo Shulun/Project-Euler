22858ab911e2e38e5e730bc622fd3b082aaba6b3
Shulun <chenshulun@gmail.com>
2016-11-03 13:10:48 -0400

    Problem 20, executed in 0.04636 sec

---
+++ problem20.m
@@ -0,0 +1,38 @@
+close all; clear; clc;
+
+% This method yields 683 which is probably 
+% due to precision issue
+%{
+tic
+number = sym(factorial(100));
+numStr = char(number);
+
+n = length(numStr);
+tot = 0;
+for i = 1:n
+    tot = tot + str2num(numStr(i));
+end
+tot
+toc
+%}
+
+% Use Java mehtods to solve this problem
+tic
+result = java.math.BigDecimal(1);
+for i = 1:100
+    result = result.multiply(java.math.BigDecimal(i));
+end
+
+num = result.toString();
+numVal = java.lang.Character('a');
+
+tot = 0;
+n = length(num);
+for i = 0:n-1
+    tot = tot + numVal.getNumericValue(num.charAt(i));
+end
+tot
+toc
+
+
+
