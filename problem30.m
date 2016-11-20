449adb4959a41af5bd4fd3308badc9b956286836
Shulun <chenshulun@gmail.com>
2016-11-19 23:51:36 -0500

    Problem 30. executed in 0.3418 sec

---
+++ problem30.m
@@ -0,0 +1,18 @@
+close all; clear; clc;
+
+tic
+result = 0;
+for i = 2:355000
+    sumDig = 0;
+    num = i;
+    while num > 0
+        d = mod(num,10);
+        num = floor(num/10);
+        sumDig = sumDig + d^5;        
+    end
+    if sumDig == i
+        result = result + i;
+    end
+end
+result
+toc
